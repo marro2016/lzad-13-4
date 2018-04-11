11b87da09aac7319808378fb1b38822ac87b0cf8
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-04-11 23:27:51 +0200

    Add

--- program.js
+++ program.js
@@ -1,29 +1,29 @@
-var OSinfo = require('./modules/OSInfo');
-var time2 = require('./modules/time');
+var OSinfo = require('../modules/OSinfo');
 
 process.stdin.setEncoding('utf-8');
-console.log('App started, please write instruction below...');
+
+
 process.stdin.on('readable', function() {
   var input = process.stdin.read();
   if (input !== null) {
+    process.stdout.write(input);
     var instruction = input.toString().trim();
     switch (instruction) {
+      case 'version':
+        process.stdout.write('Node version: ' + process.versions.node + '\n');
+        break;
+      case 'language':
+        process.stdout.write('Language is: ' + process.env.LANG + '\n');
+        break;
       case '/exit':
         process.stdout.write('Quitting app!\n');
         process.exit();
         break;
-      case '/sayhello':
-        process.stdout.write('hello\n');
-        break;
       case '/getOSinfo':
         OSinfo.print();
-        break;
-      case '/czas':
-        time2.time();
         break;
       default:
         process.stderr.write('Wrong instruction!\n');
     }
   }
 });
-
